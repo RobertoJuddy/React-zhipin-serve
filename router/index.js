04fee3699a5e847acdacb4e5b692138f9c81d1d8
beto <849885262@qq.com>
2018-11-01 17:21:38 +0800

    xx

--- router/index.js
+++ router/index.js
@@ -14,9 +14,7 @@ router.use(express.urlencoded({extended: true}));
 
 //登录页面
 router.post('/login', async (req, res) => {
-
   const {username, password} = req.body;
-
   if (!username || !password) {
     res.json({
       "code": 2,
@@ -27,7 +25,6 @@ router.post('/login', async (req, res) => {
 
   try {
     const data = await User.findOne({username, password: md5(password)});
-
     if (data) {
       res.json({
         "code": 0,
@@ -37,7 +34,6 @@ router.post('/login', async (req, res) => {
           "type": data.type,
         }
       })
-
     } else {
       res.json({
         "code": 1,
@@ -55,36 +51,24 @@ router.post('/login', async (req, res) => {
 
 //注册页面
 router.post('/register', (req, res) => {
-
-
   const {username, password, type} = req.body;
-
   console.log(username, password, type);
-
   if (!username || !password || !type) {
-
     res.json({
       "code": 2,
       "msg": "输入不合法"
     });
     return;
   }
-
   User.findOne({username}, (err, data) => {
-
     if (!err) {
-
       if (data) {
         res.json({
-
           "code": 1,
           "msg": "此用户已存在"
         })
-
       } else {
-
         User.create({username, password: md5(password), type}, (err, data) => {
-
           if (!err) {
             res.json({
               code: 0,
@@ -94,15 +78,12 @@ router.post('/register', (req, res) => {
                 type: data.type
               }
             })
-
           } else {
-
             res.json({
               "code": 3,
               "msg": "网络不稳定，请刷新"
             })
           }
-
         })
       }
     } else {
